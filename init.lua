0353174b0ce383215520e0b073acd35e58579f7b
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2024-06-12 12:51:29 -0700

    extension of last commit

--- init.lua
+++ init.lua
@@ -7,16 +7,12 @@ require('plugins.lazy')
 require('plugins.nvim-tree')
 require('plugins.telescope')
 require('plugins.nvim-treesitter')
-require('plugins.lsp.mason')
 
+require('plugins.lsp.mason')
 require('plugins.lsp.lspconfig')
 require('plugins.lsp.cmp')
 
 --require('plugins.themes.vscode')
 
 require('plugins.themes.catpuccin')
-
---require('plugins.bufferline')
-
---require('plugins.feline')
 
