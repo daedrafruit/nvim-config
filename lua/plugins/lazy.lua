86beb1f8c6347520342ddca401b36b2b048b6110
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2024-06-13 10:32:34 -0700

    removed bufferline

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -13,32 +13,26 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 
-{"nvim-tree/nvim-tree.lua", version = "*", lazy = false, dependencies = { "nvim-tree/nvim-web-devicons", }},
+	{"nvim-tree/nvim-tree.lua", version = "*", lazy = false, dependencies = { "nvim-tree/nvim-web-devicons", }},
 
-{'nvim-telescope/telescope.nvim', tag = '0.1.6', dependencies = { 'nvim-lua/plenary.nvim' }},
+	{'nvim-telescope/telescope.nvim', tag = '0.1.6', dependencies = { 'nvim-lua/plenary.nvim' }},
 
-{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+	{ "williamboman/mason.nvim", dependencies = { 'williamboman/mason-lspconfig.nvim' }},
+	{'neovim/nvim-lspconfig'},
+	{'hrsh7th/nvim-cmp', dependencies = {
+		'hrsh7th/cmp-nvim-lsp',
+		'hrsh7th/cmp-buffer',
+		'hrsh7th/cmp-path',
+		'hrsh7th/cmp-cmdline',
+		'L3MON4D3/LuaSnip',
+		'saadparwaiz1/cmp_luasnip',
+		'hrsh7th/cmp-vsnip',
+		'hrsh7th/vim-vsnip'
+	}},
 
-{ "williamboman/mason.nvim", dependencies = { 'williamboman/mason-lspconfig.nvim' }},
-
-{'neovim/nvim-lspconfig'},
-
-  {'hrsh7th/nvim-cmp', dependencies = {
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-cmdline',
-    'L3MON4D3/LuaSnip',
-    'saadparwaiz1/cmp_luasnip',
-    'hrsh7th/cmp-vsnip',
-    'hrsh7th/vim-vsnip'
-  }},
-
-{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-
-{ 'Mofiqul/vscode.nvim' },
-
-{'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{ 'Mofiqul/vscode.nvim' },
 
 })
 
