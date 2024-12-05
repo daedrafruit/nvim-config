8e66f95aec145e43126819ffa6b8898c3f9a8917
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2024-12-05 14:02:11 -0700

    maybe fixed lua lsp

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -25,3 +25,14 @@ lspconfig.pyright.setup {
   end,
 }
 
+lspconfig.lua_ls.setup{
+  on_attach = function(client, bufnr)
+    local opts = { noremap=true, silent=true }
+    vim.keymap.set('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    vim.keymap.set('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    vim.keymap.set('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  end,
+}
