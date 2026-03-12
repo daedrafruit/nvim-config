724dc30c872e4718e145c768c5f0b10a925eb531
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2026-03-12 14:11:52 -0700

    slight cleanup for matchup

--- lua/daedr/plugins/vim-matchup.lua
+++ lua/daedr/plugins/vim-matchup.lua
@@ -1,25 +1,23 @@
 return {
   'andymass/vim-matchup',
   init = function()
-    -- modify your configuration vars here
     vim.g.matchup_treesitter_stopline = 500
 
-    -- or call the setup function provided as a helper. It defines the
-    -- configuration vars for you
+    -- hack to prevent breaking cmp tab bind
     local ok, cmp = pcall(require, "cmp")
     if ok then
       cmp.event:on("menu_opened", function() vim.b.matchup_matchparen_enabled = false end)
       cmp.event:on("menu_closed", function() vim.b.matchup_matchparen_enabled = true end)
     end
+    -- end of hack
+
     require('match-up').setup({
       treesitter = {
         stopline = 500
       }
     })
   end,
-  -- or use the `opts` mechanism built into `lazy.nvim`. It calls
-  -- `require('match-up').setup` under the hood
-  ---@type matchup.Config
+
   opts = {
     treesitter = {
       stopline = 500,
