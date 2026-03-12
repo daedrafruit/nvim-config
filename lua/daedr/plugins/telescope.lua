03ddaa61832e8f394e495010fbe5b0ac05284cc3
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2026-03-12 01:41:31 -0700

    reverted telescope keybind

--- lua/daedr/plugins/telescope.lua
+++ lua/daedr/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	keys = {
     { '<leader>ff', function() require('telescope.builtin').find_files() end },
-    { '<leader>ft', function() require('telescope.builtin').live_grep() end },
+    { '<leader>fg', function() require('telescope.builtin').live_grep() end },
     { '<leader>fb', function() require('telescope.builtin').buffers() end },
     { '<leader>fh', function() require('telescope.builtin').help_tags() end },
     { '<leader>fr', function() require('telescope.builtin').oldfiles() end },
