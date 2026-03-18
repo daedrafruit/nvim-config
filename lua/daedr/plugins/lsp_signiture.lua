23a2941649397820ce34d3ffda003359c26bfd90
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2026-03-17 17:01:31 -0700

    added lsp signiture plugin

---
+++ lua/daedr/plugins/lsp_signiture.lua
@@ -0,0 +1,11 @@
+return {
+  "ray-x/lsp_signature.nvim",
+  event = "InsertEnter",
+  opts = {
+    hint_enable = false,
+    doc_lines = 0,
+    handler_opts = {
+      border = "single"
+    }
+  },
+}
