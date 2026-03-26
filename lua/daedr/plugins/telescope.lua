d4f44f34a0a76761ed3bc9b2406aba7f38f8dcf2
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2026-03-26 13:32:12 -0700

    modified telescope/harpoon

--- lua/daedr/plugins/telescope.lua
+++ lua/daedr/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
     { '<leader>ff', function() require('telescope.builtin').find_files() end },
     { '<leader>fg', function() require('telescope.builtin').live_grep() end },
     { '<leader>fb', function() require('telescope.builtin').buffers() end },
-    { '<leader>fh', function() require('telescope.builtin').help_tags() end },
+    { '<leader>f?', function() require('telescope.builtin').help_tags() end },
     { '<leader>fr', function() require('telescope.builtin').oldfiles() end },
     { '<leader>fs', function() require('telescope.builtin').lsp_document_symbols() end },
 
