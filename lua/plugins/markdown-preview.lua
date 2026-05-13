d688659ac562eb35b272d9c5f43af0a67e7e6fc0
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2026-05-12 18:45:50 -0700

    fixed markdown preview

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -2,26 +2,30 @@ vim.pack.add {
   "https://github.com/iamcco/markdown-preview.nvim",
 }
 
--- Disable default browser handling
+local app_dir = vim.fn.stdpath("data") .. "/site/pack/core/opt/markdown-preview.nvim/app/node_modules"
+if vim.fn.isdirectory(app_dir) == 0 then
+  vim.defer_fn(function()
+    vim.fn["mkdp#util#install"]()
+  end, 100)
+end
+
 vim.g.mkdp_browser = ""
 
--- Custom browser function (Lua wrapper around Vimscript)
 vim.cmd([[
   function! OpenMarkdownPreview(url)
-    execute "silent ! chromium --app=" . a:url . " &"
+    call jobstart(["firefox", "--new-window", a:url])
   endfunction
 ]])
 
 vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
 
--- Your existing settings (unchanged)
 vim.g.mkdp_auto_start = 0
 vim.g.mkdp_auto_close = 1
 vim.g.mkdp_refresh_slow = 0
 vim.g.mkdp_command_for_global = 0
 vim.g.mkdp_open_to_the_world = 0
 vim.g.mkdp_open_ip = ""
-vim.g.mkdp_echo_preview_url = 0
+vim.g.mkdp_echo_preview_url = 1
 
 vim.g.mkdp_preview_options = {
   disable_sync_scroll = 0,
@@ -32,4 +36,3 @@ vim.g.mkdp_preview_options = {
 }
 
 vim.g.mkdp_theme = "dark"
-
